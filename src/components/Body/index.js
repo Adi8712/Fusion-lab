62757665287179294e2b5cca1a7014df87e28d52
Dhruv Bhanushali <119250923+theDRB123@users.noreply.github.com>
2024-08-16 10:53:21 +0530

    Fixed modules and Code Cleanup

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import Header from '../Header'
-import Courses from '../Courses'
+import Modules from '../Modules'
 import CourseList from '../Courselist'
 import "./index.css"
 import TestSchedule from '../TestSchedule'
 
-const courseDetails = [
-    { id: 1, total: 2, text: 'Current Courses ', color: 'orange' },
-    { id: 2, total: 5, text: 'Completed Courses', color: 'green', },
-    { id: 3, total: 10, text: "Interested Courses", color: 'blue' },
+const modules = [
+    { id: 1, total: 2, text: 'Acadamics', color: 'orange' },
+    { id: 2, total: 5, text: 'Mess Management', color: 'green', },
+    { id: 3, total: 10, text: 'Healthcare Center', color: 'blue' },
+    { id: 4, total: 10, text: 'Scholarship', color: 'blue' },
 ];
 
 const Body = () => {
     return (
         <div className='w-100 course-wrapper'>
             <Header />
-            <h4 className='mx-3 d-block'>Welcome Back, John</h4>
+            <h4 className='mx-3 d-block'>Welcome Back, User</h4>
             <div className='d-flex justify-content-around'>
                 <div>
                     <div className='d-flex gap-5 mt-4 '>
-                        {courseDetails.map(each => (
-                            <Courses key={each.id} each={each} />
+                        {modules.map(m => (
+                            <Modules key={m.id} each={m} />
                         ))}
                     </div>
                     <div>
--- src/components/Modules/index.js
+++ src/components/Modules/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import "./index.css"
 
 
-const Courses = (props) => {
+const Modules = (props) => {
     const { each } = props
     const { total, text, color } = each
     const colorText = {
@@ -11,10 +11,10 @@ const Courses = (props) => {
     }
     return (
         <div className='d-flex flex-column align-items-center shadow course-container'>
-            <p className='fs-2' style={colorText}>{total} </p>
+            <p className='fs-2' style={colorText}></p>
             <p>{text} </p>
         </div>
     )
 }
 
-export default Courses
+export default Modules
